209fec1401da44e88b09ae6a8769d3c228e9a0f2
gerryno17 <geraldoqoku87@gmail.com>
2023-12-07 07:42:00 +0000

    primo commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+
+
+
+          Matrice m = new Matrice ("TPSIT");
+            Vigenere v= New Vigenere;
+          Vigenere (8,12,0,5,m);
+            Thread t= New thread (v);
+            t.start();
+         }
+
+       
+}
